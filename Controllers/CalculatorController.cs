e8f18e4819a9e82e7e3abff145049185e88535b8
Michel <michelmachado11@hotmail.com>
2022-09-13 15:18:51 -0300

    read me

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
+
+namespace mvc.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Soma(int valor1, int valor2) { 
+
+        var total = valor1 + valor2;
+        return Ok(total);
+        }
+        public IActionResult Subtrai(int valor1, int valor2)
+        {
+
+            var total = valor1 - valor2;
+            return Ok(total);
+        }
+        public IActionResult Multiplica(int valor1, int valor2)
+        {
+
+            var total = valor1 * valor2;
+            return Ok(total);
+        }
+        public IActionResult Divide(int valor1, int valor2)
+        {
+
+            var total = valor1 / valor2;
+            return Ok(total);
+        }
+    }
+}
---
+++ wwwroot/js/site.js
@@ -0,0 +1,57 @@
+﻿function soma() {
+    let valor1 = $('#valor1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculator/Soma",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    });
+}
+
+function subtrai() {
+    let valor1 = $('#valor1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculator/Subtrai",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    })
+}
+function divide() {
+    let valor1 = $('#valor1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculator/Divide",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    })
+}
+function multiplica() {
+    let valor1 = $('#valor1').val();
+    let valor2 = $('#valor2').val();
+    $.ajax({
+        url: "/Calculator/Multiplica",
+        data: {
+            valor1: valor1,
+            valor2: valor2
+        },
+
+    }).done(function (response) {
+        $('#total').val(response);
+    })
+}
